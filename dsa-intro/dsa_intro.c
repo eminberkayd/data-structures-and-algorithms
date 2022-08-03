58e02bccd6266b94ae39471c1b865ac7fd23e22b
emin-berkay-daglar <eminberkaydaglar@gmail.com>
2022-08-04 00:02:56 +0300

    first commit

---
+++ dsa-intro/dsa_intro.c
@@ -0,0 +1,25 @@
+/*
+my notes on dsa - taken from programiz.com
+
+Depending on your requirement and project, it is important to choose the right data structure for your project.
+
+Basically, data structures are divided into two categories:
+* Linear data structures : In linear data structures, the elements are arranged in sequence one after the other. The time complexity increase with the data size.
+EX: array, stack, queue, linked list.
+
+* Non-linear data structures : Unlike linear data structures, elements in non-linear data structures are not in any sequence. Instead they are arranged in a hierarchical manner where one element will be connected to one or more elements. Time complexity remains the same.
+EX : graph => In graph data structure, each node is called vertex and each vertex is connected to other vertices through edges.
+   : tree.
+
+---
+Generally, software development involves learning new technologies on a daily basis. You get to learn most of these technologies while using them in one of your projects. However, it is not the case with algorithms.
+
+If you don't know algorithms well, you won't be able to identify if you can optimize the code you are writing right now. You are expected to know them in advance and apply them wherever possible and critical.
+---
+The study of change in performance of the algorithm with the change in the order of the input size is defined as asymptotic analysis.
+- Big-O -> Worst Case
+- Omega -> Best Case
+- Theta -> Average Case
+
+
+*/
